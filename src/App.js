b33399d30e411375c2e26b6a4ddde906b69d551c
Administrator <administrator@adminisatorsmbp.in.ibm.com>
2019-08-20 16:28:53 +0530

    added AWS Keys

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1> Welcome to first Docker image with React</h1>
+          Welcome to first Docker image with React
         </p>
         <a
           className="App-link"
