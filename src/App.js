bff95c835a42ece25f532d775be957b33378db6d
Administrator <administrator@adminisatorsmbp.in.ibm.com>
2019-08-20 16:51:16 +0530

    added feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to first Docker image with React
+          Changed to feature branch
         </p>
         <a
           className="App-link"
